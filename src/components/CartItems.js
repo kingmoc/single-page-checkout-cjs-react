3bbecf3a00ded832d321a9d55eebe283fe8a6ebc
kingmoc <joe.c.a.garcia@gmail.com>
2020-02-28 11:36:19 -0600

    Added Quanity Action Buttons and Input / Added red Circle for delete

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Item, Header } from 'semantic-ui-react';
+import { Item, Header, Button, Icon, Input } from 'semantic-ui-react';
 
 const CartItems = (props) => {
 
@@ -7,13 +7,32 @@ const CartItems = (props) => {
 
     return (
         <>
+            <Button className='delete-item' negative circular icon='x' />
             <Item.Image size='tiny' src={props.item.media.source} />
             <Item.Content verticalAlign='middle'>
                 <Item.Header>{props.item.name}</Item.Header>
                 <Item.Meta>
                     <span>{props.item.variants[0].option_name}</span>
                 </Item.Meta>
-                <Item.Extra>
+                {/* <Button.Group>
+                    <Button>
+                        <Icon name='play' />
+                    </Button>
+                    <Input className='input-quanity' focus size='large'/>
+                    <Button>
+                        <Icon name='shuffle' />
+                    </Button>
+                </Button.Group> */}
+                <div className='quanity-group'>
+                    <Button className='quan-buttons'> <Icon name='minus' /> </Button>
+                    <Input 
+                        className='input-quanity'
+                        value={props.item.quantity} 
+                    />
+                    <Button className='quan-buttons'> <Icon name='plus' /> </Button>
+                </div>
+                {/* <Button circular icon='x' /> */}
+                <Item.Extra className='item-total'>
                     <Header floated='right'>${props.item.price.formatted_with_symbol}</Header>
                 </Item.Extra>
             </Item.Content>
